1d63b5bc355ba7e8ecd422fa0dd97248a7054ea7
Davi Antonio Ramos Araujo <124791732+davizin-ra@users.noreply.github.com>
2026-04-28 14:13:59 -0300

    Introdução ao C#

---
+++ Aula03/Exercícios/README.md
@@ -0,0 +1,3 @@
+# Atividade:
+
+https://sparkling-link-662.notion.site/Atividade-Sintaxe-C-34c1683796128059b8efc3ef2eff1b39
---
+++ Aula03/Introdução/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace InicioCsharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+
+        /*
+            // int idade  = 18;
+            // double altura = 1.75;
+            // string nome = "Davizin";
+            // bool ativo = true;
+
+            // Console.WriteLine(idade);
+            // Console.WriteLine(altura);
+            // Console.WriteLine(nome);
+            // Console.WriteLine(ativo);
+
+            // Console.WriteLine("Digite seu nome: ");
+            // string nome = Console.ReadLine();
+            // Console.WriteLine("Digite sua idade: ");
+            // int idade = int.Parse(Console.ReadLine());
+        */
+
+
+        //Contatenar
+
+        /*
+            // Console.WriteLine("Nome: " + nome);
+            // Console.WriteLine("Idade: " + idade);
+        */
+
+
+        // // Interpolar
+
+        /*
+            Console.WriteLine($"Olá, eu sou {nome}, tenho {idade} anos");
+
+            int n1 = 10;
+            int n2 = 5;
+            int soma = n1 + n2;
+
+            Console.WriteLine("Soma: " + soma);
+        */
+
+        //Operadores Relacionais
+
+        /*
+            int idade = 18;
+
+            bool maiorDeIdade = idade >= 18;
+            bool menorDeIdade = idade < 18;
+
+            Console.WriteLine("Maior de idade: " + maiorDeIdade);
+            Console.WriteLine("Menor de idade: " + menorDeIdade);
+        */
+
+        //Operadores lógicos
+
+        /*
+            bool condicao1 = true;
+            bool condicao2 = false;
+
+            Console.WriteLine(condicao1 && condicao2);
+            Console.WriteLine(condicao1 || condicao2);
+            Console.WriteLine(!condicao1);
+
+
+            int idade = 20;
+            bool possuiCarteira = false;
+
+            bool podeDirigir = idade >= 18 && possuiCarteira;
+            Console.WriteLine("Pode dirigir? " + podeDirigir);
+
+
+            int numero = 5;
+            bool resultado = numero < 0 || numero > 10;
+
+            Console.WriteLine("Fora do intervalo 0 a 10: " + resultado);
+        
+            bool estaChovendo = false;
+            Console.WriteLine(!estaChovendo);
+
+        */
+
+    }
+}
