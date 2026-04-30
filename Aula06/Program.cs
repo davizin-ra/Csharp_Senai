1110383a04cdb1a03ea034bfd19741be47c4f8c0
Davi Antonio Ramos Araujo <124791732+davizin-ra@users.noreply.github.com>
2026-04-30 15:10:44 -0300

    Estrutura de repetição

---
+++ Aula06/Program.cs
@@ -0,0 +1,177 @@
+﻿namespace Aula06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    //! Fácil
+
+    /* -- 1
+        for (int i = 1; i <= 10; i++){
+            Console.WriteLine(i);
+        }
+    */
+
+    /* -- 2
+        for (int i = 10; i >= 0; i--){
+            Console.WriteLine(i);
+        }
+    */
+
+    /* -- 3
+        Console.WriteLine("Digite um número");
+        int num = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= 10; i++){
+            Console.WriteLine(i * num);
+        }
+    */
+
+    /* -- 4
+        int num = 1;
+
+        while (num <= 5){
+            Console.WriteLine(num);
+            num++;
+        }
+    */
+
+    /* -- 5
+        int soma = 0;
+
+        for(int i = 0; i <= 5; i++){
+            soma += i;
+        }
+
+        Console.WriteLine("Soma: " + soma);
+    */
+
+    //! Médio
+
+    /* -- 1
+        int soma = 0;
+
+        for(int i = 1; i <= 5; i++){
+            Console.WriteLine("Digite um número");
+            int num = int.Parse(Console.ReadLine());
+
+            soma += num;
+        }
+
+        Console.WriteLine("Soma: " + soma);
+    */
+
+    /* -- 2
+        for(int i = 0; i <= 20; i += 2){
+            Console.WriteLine(i);
+        }
+    */
+
+    /* -- 3
+        Console.WriteLine("Digite a senha");
+        string senha = Console.ReadLine();
+
+        while(senha != "1234"){
+            Console.WriteLine("Digite a senha");
+            senha = Console.ReadLine();
+        }
+    */
+
+    /* -- 4
+        Console.WriteLine("1 - Ver mensagem \n2 - Sair");
+        string opcao = Console.ReadLine();
+
+        do {
+            Console.WriteLine("\nMensagem!!\n");
+            Console.WriteLine("1 - Ver mensagem \n2 - Sair\n");
+            opcao = Console.ReadLine();
+        }
+        while(opcao != "2");
+        Console.WriteLine("Você saiu");
+    */
+
+    /* -- 5
+        int soma = 0;
+
+        for(int i = 1; i <= 4; i++){
+            Console.WriteLine("Digite um valor");
+            int num = int.Parse(Console.ReadLine());
+
+            soma += num;
+        }
+
+        Console.WriteLine("Média: " + soma/4);
+    */
+
+    //! Difícil
+
+    /* -- 1
+        int maior = 0;
+
+        for(int i = 1; i <= 5; i++){
+            Console.WriteLine("\nDigite um valor positivo");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num > maior){
+                maior = num;
+            }
+            else if(num < 0){
+                Console.WriteLine("\nNúmero inválido");
+            }
+        }
+        Console.WriteLine("\nO maior número é: " + maior);
+    */
+
+    /* -- 2
+        int contador = 0;
+
+        Console.WriteLine("Digite 0");
+        string num = Console.ReadLine();
+
+        while(num != "0"){
+            Console.WriteLine("\nDigite 0");
+            num = Console.ReadLine();
+            contador ++;
+        }
+
+        Console.WriteLine($"\nVocê errou {contador} vezes.");
+    */
+
+    /* -- 3
+        Console.WriteLine("Digite um número positivo");
+        int num = int.Parse(Console.ReadLine());
+        int soma = num;
+
+        while(num >= 0){
+            Console.WriteLine("Digite um número positivo");
+            num = int.Parse(Console.ReadLine());
+            soma = num > 0 ? soma + num : soma + 0;
+        }
+
+        Console.WriteLine($"\nProcesso finalizado, você digitou um número negativo.");
+        Console.WriteLine($"\nSoma: {soma}");
+    */
+
+    /* -- 4
+        int[] lista = {1, 2, 3, 4, 5};
+
+        foreach (int i in lista){
+            Console.WriteLine(i);
+        }
+    */
+
+    /* -- 5
+        int[] lista = {60, 10, 10, 10, 10};
+        int soma = 0;
+
+        foreach (int i in lista){
+            soma += i;
+        }
+
+        Console.WriteLine($"Média: {soma/5}");
+    */
+
+    }
+}
+
---
+++ Aula06/README.md
@@ -0,0 +1,37 @@
+https://sparkling-link-662.notion.site/Exerc-cios-de-Repeti-o-35116837961280569be4defa4eca8e8a
+
+# Estruturas de repetição
+
+## For
+
+    for (int i = 1; i <= 5; i++){
+        Console.WriteLine(i);
+    }
+
+## While
+
+    int contador = 1;
+
+    while (contador <= 5){
+        Console.WriteLine(contador);
+        contador++;
+    }
+
+### Do/While
+
+    int num = 5;
+
+    do
+    {
+        Console.WriteLine("Digite um número");
+        num = int.Parse(Console.ReadLine());
+    }
+    while (num <= 0);
+
+## Foreach
+
+    string[] nomes = {"Da", "Go", "Lu", "Pe", "Cin", "Wes" };
+
+    foreach(string nome in nomes){
+        Console.WriteLine(nome);
+    }
