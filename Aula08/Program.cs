f30492446fbc48761417651c2e5fdbf99883e748
Davi Antonio Ramos Araujo <124791732+davizin-ra@users.noreply.github.com>
2026-05-04 17:29:15 -0300

    POO

---
+++ Aula08/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Carro meuCarro = new Carro();   
+
+        meuCarro.Marca = "Toyota";
+        meuCarro.Modelo = "Corolla";
+        meuCarro.Ano = 2022;
+
+        meuCarro.ExibirInformacoes();
+
+        Moto minhaMoto = new Moto();
+
+        minhaMoto.Marca = "Honda";
+        minhaMoto.Ano = 2021;
+        minhaMoto.TemPartidaEletrica = true;
+
+        minhaMoto.ExibirDetalhesMoto();
+    }
+}
