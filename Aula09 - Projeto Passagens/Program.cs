eead5b293884778c2c8b307d78a4af904ae09dd0
Davi Antonio Ramos Araujo <124791732+davizin-ra@users.noreply.github.com>
2026-05-06 17:26:04 -0300

    Projeto Passagens Aéreas

---
+++ Aula09 - Projeto Passagens/Program.cs
@@ -0,0 +1,34 @@
+﻿using Aula09___Projeto_Passagens.models.auth;
+using Aula09___Projeto_Passagens.models;
+
+namespace Aula09___Projeto_Passagens;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Login login = new Login();
+        Console.WriteLine($"\nDigita a sua senha:");
+        string digitado = Console.ReadLine();
+        login.fazerLogin(digitado);
+
+        while (!login.autenticado){
+            Console.Clear();
+            Console.WriteLine($"\nSenha incorreta, tente novamente:");
+            digitado = Console.ReadLine();
+            login.fazerLogin(digitado);
+        }
+
+        Console.Clear();
+        Console.WriteLine($"\nBem vindo ao agendador de passagens!!");
+        
+        List<Passageiro> passageiros = new List<Passageiro>();
+
+        passageiros.Add(new Passageiro("asd", "sss", "aaa", "dsd"));
+
+        foreach (Passageiro p in passageiros){
+            p.verPassageiro();
+        }
+    }
+}
